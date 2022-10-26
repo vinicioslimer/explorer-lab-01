c8f2c4e95292fc895cccf4529f669e8d54b4fea2
Vinicios Limer <vinicioslimer@outlook.com>
2022-10-25 23:17:59 -0300

    Mask cards patterns

--- src/main.js
+++ src/main.js
@@ -33,14 +33,43 @@ const expirationDatePattern = {
   blocks: {
     YY: {
       mask: IMask.MaskedRange,
-      from: new Date().getFullYear,
-      to: 
-    }
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() +10).slice(2)
+    },
     MM: {
-      mask: Imask.MaskedRange,
+      mask: IMask.MaskedRange,
       from: 1,
       to: 12
     },
   },
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex:/^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:/^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2}\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function(appended, dynamicMasked) {
+          let number = (dynamicMasked.value + appended).replace(/\D/g, "")
+          let foundMask = dynamicMasked.compiledMasked.find(function (item) {
+            return number.match(item.regex)
+          })
+          
+          return foundMask
+  },
+}
+let cardNumberMasked = IMask(cardNumber, cardNumberPattern)
